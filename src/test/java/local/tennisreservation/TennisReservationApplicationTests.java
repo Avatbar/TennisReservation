4df67e9b3e4737252fdd73c2134d07172db0d00e
Avatbar <tadeasmachacek@gmail.com>
2023-05-31 01:31:17 +0200

    initial commit

---
+++ src/test/java/local/tennisreservation/TennisReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package local.tennisreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
